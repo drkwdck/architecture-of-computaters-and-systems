98bb5b345949110692ae3388bbf65b3bb5d69681
drkwdck <reshetnikov.e.a@mail.ru>
2020-09-10 23:48:42 +0300

    доделал лабу

--- lab_1_05.cpp
+++ lab_1_05.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <bitset>
 
-void print_four_chars(char some_str[4])
+void print_four_chars(char some_str[])
 {
     for (int i = 0; i < 4; i++)
     {
@@ -17,10 +17,39 @@ int main()
     std::bitset<32> x_bin(x);
 
     std::cout << std::dec << x << std::endl;
-    std::cout << x_bin;
+    std::cout << x_bin << std::endl;
 
-    char eng_string[4] = {'a', 'b', 'c', 'd'};
-    char rus_string[4] = {'а', 'б', 'в', 'г'};
+    char eng_string[] = "abcd";
+    char rus_string[] = "абвг";
+
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << (int)rus_string[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << (int)eng_string[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    wchar_t expanded_eng_string[] = L"abcd";
+    wchar_t expanded_rus_string[] = L"абвг";
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << (int)expanded_eng_string[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << (int)expanded_rus_string[i] << " ";
+    }
 
     return 0;
 }
