8c68a3826f2395ed91198ddf9fc03bdc061d6e5d
drkwdck <reshetnikov.e.a@mail.ru>
2020-09-21 20:54:26 +0300

    начал отчеты по 1-2 лабе

--- 1/lab_1_05.cpp
+++ 1/lab_1_05.cpp
@@ -12,25 +12,32 @@ void print_four_chars(char some_str[])
 
 int main()
 {
+    std::cout <<"Задание 6. Определите и выпишите в отчёт, как хранятся в памяти компьютера:\n";
+    std::cout <<"– целое число 0x12345678; по результату исследования определите порядок\n";
+    std::cout <<"следования байтов в словах для вашего процессора:\n";
+    std::cout <<"– Little-Endian (от младшего к старшему, порядок Intel)\n";
+    std::cout <<"– Big-Endian (от старшего к младшему, порядок Motorola)\n";
+    std::cout <<"– строки 'abcd' и 'абвг' (массив из char);\n";
+    std::cout <<"– «широкие» строки L'abcd' и L'абвг' (массив из wchar_t).\n\n";
     // 6
     int32_t x = 0x12345678;;
     std::bitset<32> x_bin(x);
 
-    std::cout << std::dec << x << std::endl;
-    std::cout << x_bin << std::endl;
+    std::cout << "0x12345678\n";
+    std::cout << "dec: "<< std::dec << x << std::endl;
+    std::cout << "binary: "<< x_bin << std::endl;
 
     char eng_string[] = "abcd";
     char rus_string[] = "абвг";
 
-    
-
+    std::cout << "\n abcd:\n";
     for (int i = 0; i < 8; i++)
     {
         std::cout << (int)rus_string[i] << " ";
     }
 
-    std::cout << std::endl;
 
+    std::cout << "\nабвг\n";
     for (int i = 0; i < 4; i++)
     {
         std::cout << (int)eng_string[i] << " ";
@@ -41,12 +48,14 @@ int main()
     wchar_t expanded_eng_string[] = L"abcd";
     wchar_t expanded_rus_string[] = L"абвг";
 
+    std::cout << "\nL\"abcd\"\n";
+
     for (int i = 0; i < 4; i++)
     {
         std::cout << (int)expanded_eng_string[i] << " ";
     }
 
-    std::cout << std::endl;
+    std::cout << "\nL\"абвг\"";
 
     for (int i = 0; i < 4; i++)
     {
--- 1/lab_1_2.cpp
+++ 1/lab_1_2.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <cstddef>
+#include <string>
+
+template<typename T>
+void print_size(std::string type_name)
+{
+    std::cout << type_name <<": " << sizeof(T) << std::endl;
+}
 
 int main()
 {
-    // 7
-    std::cout << "char: " << sizeof(char) << std::endl;
-    std::cout << "bool: " << sizeof(bool) << std::endl;
-    std::cout << "wchar_t: " << sizeof(wchar_t) << std::endl;
-    std::cout << "short: " << sizeof(short) << std::endl;
-    std::cout << "int: " << sizeof(int) << std::endl;
-    std::cout << "long: " << sizeof(long) << std::endl;
-    std::cout << "long long: " << sizeof(long long) << std::endl;
-    std::cout << "float: " << sizeof(float) << std::endl;
-    std::cout << "double: " << sizeof(double) << std::endl;
-    std::cout << "long double: " << sizeof(long double) << std::endl;
-    std::cout << "size_t: " << sizeof(size_t) << std::endl;
-    std::cout << "ptrdiff_t: " << sizeof(ptrdiff_t) << std::endl;
-    std::cout << "void*: " << sizeof(void*) << std::endl;
+    std::cout << "Задание 7. При помощи оператора sizeof выясните, сколько байтов\n";
+    std::cout << "занимают переменные следующих типов: char, bool, wchar_t, short, int,\n";
+    std::cout << "long, long long, float, double, long double, size_t, ptrdiff_t,\n";
+    std::cout << "void*. Результаты оформите в отчёте в виде таблицы, указывая для каждого типа\n";
+    std::cout << "его назначение.\n";
+    std::cout << "Для выполнения единообразных действий над переменными различных типов\n";
+    std::cout << "используются макросы препроцессора C или шаблоны C++.\n";
+    std::cout << "Проверьте, соответствуют ли размеры типов современному стандарту C++.\n\n";
+    print_size<char>("char");
+    print_size<bool>("bool");
+    print_size<wchar_t>("wchar_t");
+    print_size<short>("short");
+    print_size<int>("int");
+    print_size<long>("long");
+    print_size<long long>("long long");
+    print_size<float>("float");
+    print_size<double>("double");
+    print_size<long double>("long double");
+    print_size<size_t>("size_t");
+    print_size<ptrdiff_t>("ptrdiff_t");
+    print_size<void*>("void*");
 
     return 0;
 }
