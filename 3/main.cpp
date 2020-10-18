6fab29dcebc2cccfea662fdadaa5e6cdedc05559
drkwdck <reshetnikov.e.a@mail.ru>
2020-10-18 21:44:35 +0300

    поправил вывод в 1 задании

--- 3/main.cpp
+++ 3/main.cpp
@@ -5,9 +5,10 @@ extern "C" int func(int x);
 
 int main()
 {
-        for (int i = 0; i > -10; i--)
+        for (int i = 0; i < 10; i++)
         {
-                printf("%d ",func(i));
+                printf("x = %d y(x) = %d ",i, func(i));
+                printf("\n");
         }
 
         return 0;
